a932f2472ee49007fe2f5d9195560b37b9bae980
Jason Chilcott <jasonchilcott@me.com>
2020-08-06 21:04:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,18 +2,17 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   # write your code here!
-  final_results = data.each_with_object({}) do |(key, value), final_array|
+  final_results = data.each_with_object({}) do |(key,value), final_array|
     value.each do |inner_key, names|
-      # enumerating over each name
       names.each do |name|
         if !final_array[name]
           final_array[name] = {}
-        end
+        end  
         if !final_array[name][key]
-          !final_array[name][key] = []
-        end
-        final_array[name][key].push(inner_key.to_s)
-      end
-    end
-  end
+          final_array[name][key] = []
+        end  
+        final_array[name][key] = final_array[name][key][inner_key.to_s]
+      end  
+    end  
+  end  
 end
